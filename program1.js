3a3e6e734d2856d545a4866a731509c46b63d38b
Sachin Mourya <94826852+sachin65M@users.noreply.github.com>
2024-01-05 13:22:08 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,37 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-};
+       const stack = [];
+    const brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
 
+    for (let char of s) {
+        if (brackets[char]) {
+            stack.push(char); 
+        } else {
+            const topOfStack = stack.pop(); 
+
+            if (brackets[topOfStack] !== char) {
+                return false; 
+            }
+        }
+    }
+
+    return stack.length === 0; 
+};
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Enter a string with parentheses: ', function (inputString) {
+    const result = isValid(inputString);
+    console.log(`The string is${result ? '' : ' not'} valid.`);
+    rl.close();
+});
 module.exports = { isValid };
 
 
