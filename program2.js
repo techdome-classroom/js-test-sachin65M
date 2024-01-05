d737da7f3c8e3ea23d8a2d58e821eff383240e47
Sachin Mourya <94826852+sachin65M@users.noreply.github.com>
2024-01-05 13:02:07 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,27 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function convertRomanToInteger(inputRoman) {
+    const romanNumeralValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let integerValue = 0;
 
-module.exports={romanToInt}
+    for (let index = 0; index < inputRoman.length; index++) {
+        const currentSymbolValue = romanNumeralValues[inputRoman[index]];
+        const nextSymbolValue = romanNumeralValues[inputRoman[index + 1]];
+
+        if (nextSymbolValue > currentSymbolValue) {
+            integerValue += nextSymbolValue - currentSymbolValue;
+            index++; // Skip the next symbol since it's already considered
+        } else {
+            integerValue += currentSymbolValue;
+        }
+    }
+
+    return integerValue;
+}
