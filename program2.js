3e1eb7e72f503d1b575f99849b27a22bb8b59fe0
Sachin Mourya <94826852+sachin65M@users.noreply.github.com>
2024-01-05 13:10:24 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,27 +1,43 @@
-function convertRomanToInteger(inputRoman) {
-    const romanNumeralValues = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
+var romanToInt = function() {
+    const readline = require('readline');
 
-    let integerValue = 0;
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-    for (let index = 0; index < inputRoman.length; index++) {
-        const currentSymbolValue = romanNumeralValues[inputRoman[index]];
-        const nextSymbolValue = romanNumeralValues[inputRoman[index + 1]];
+    rl.question('Enter a Roman numeral: ', function (inputRoman) {
+        const romanNumeralValues = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
 
-        if (nextSymbolValue > currentSymbolValue) {
-            integerValue += nextSymbolValue - currentSymbolValue;
-            index++; // Skip the next symbol since it's already considered
-        } else {
-            integerValue += currentSymbolValue;
+        let integerValue = 0;
+
+        for (let index = 0; index < inputRoman.length; index++) {
+            const currentSymbolValue = romanNumeralValues[inputRoman[index]];
+            const nextSymbolValue = romanNumeralValues[inputRoman[index + 1]];
+
+            if (nextSymbolValue > currentSymbolValue) {
+                integerValue += nextSymbolValue - currentSymbolValue;
+                index++; // Skip the next symbol since it's already considered
+            } else {
+                integerValue += currentSymbolValue;
+            }
         }
-    }
 
-    return integerValue;
-}
+        console.log('The equivalent integer value is:', integerValue);
+
+        rl.close();
+    });
+};
+
+romanToInt();
+
+
+
